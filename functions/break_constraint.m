e3ffb928de774538d18bcbdb6e6cc64036b821f9
jbrussell <jbrussell93@gmail.com>
2022-05-24 21:34:59 -0400

    fix break_constraint

--- functions/break_constraint.m
+++ functions/break_constraint.m
@@ -12,7 +12,7 @@ function [X00] = break_constraint(X00, z, z_brks)
             if I_brk-1<1 || I_brk+1>length(z)
                 continue
             end
-            X00(I_brk-1:I_brk+1,:) = 0;
+            X00(I_brk:I_brk+1,:) = 0;
         end
         if length(find(X00(I_brk,:)~=0))==2 % first derivative
             X00(I_brk,:) = 0;
