7cac22c8bd2e9ef8044a96aefa829ac2ef1d9117
Josh Russell <jbrussell93@gmail.com>
2024-02-13 19:29:33 -0500

    calc Q-corrected phv is Qp and Qs provided

--- functions/dispR_surf96.m
+++ functions/dispR_surf96.m
@@ -9,21 +9,11 @@
 % 
 function phv = dispR_surf96(vec_T,model,varargin)
 
-% jbrussell 6/16/2020: Add timeout string to surf96 inversion call to avoid
-% hanging. Get gtimeout on mac by installing coreutils with homebrew
-% >$ brew install coreutils
-% timeoutstr = 'ulimit -t 20; '; % timeout after 20 seconds of CPU time
-timeoutstr = 'gtimeout 20 '; % timeout after 20 seconds of "wall" time
-
-system('surf96 39'); % clean up
-system('rm start.mod');
-system('rm temp.dsp');
-
 if nargin==2
     nmode = 0; % default, fundamental mode
     disp_type = 'C'; % default, phase velocity
 elseif nargin==3
-	nmode = varargin{1};
+    nmode = varargin{1};
     disp_type = 'C'; % default, phase velocity
 elseif nargin==4
     nmode = varargin{1};
@@ -33,30 +23,52 @@ elseif nargin==4
     end
 end
 
-% make surf96 par file
-make_par_surf96('R',nmode);
+if size(model,2)>4 % if Qp and Qs proivded, calculate the Q-corrected phase velocity
 
-% Make dummy data
-datatemp(:,1) = vec_T(:);
-datatemp(:,2) = 3;
-datatemp(:,3)  =0.1;
+    disper = calc_amplification_ellipticity_gamma(vec_T,model,'R',nmode);
+    if strcmp(disp_type,'C')
+        phv = disper.phv;
+    elseif strcmp(disp_type,'U')
+        grv = disper.grv;
+    end
 
-writedisp_surf96(datatemp,'disp_obs.dsp','R',disp_type,1,nmode); % write temp data into dispersion file 
-writemod_surf96(model,'start.mod');
+else % Otherwise, calculate non-Q-corrected phase velocity
 
-% run surf96 
+    % jbrussell 6/16/2020: Add timeout string to surf96 inversion call to avoid
+    % hanging. Get gtimeout on mac by installing coreutils with homebrew
+    % >$ brew install coreutils
+    % timeoutstr = 'ulimit -t 20; '; % timeout after 20 seconds of CPU time
+    timeoutstr = 'gtimeout 20 '; % timeout after 20 seconds of "wall" time
 
-[~, log] = system([timeoutstr,'surf96 1 27 temp.dsp']);
-% read dispersion from tmep file
-data=readdisp_surf96('temp.dsp');
+    system('surf96 39'); % clean up
+    system('rm start.mod');
+    system('rm temp.dsp');
 
-if(~isempty(data))
-	phv=data(:,2);
-else
-	phv=[];
+    % make surf96 par file
+    make_par_surf96('R',nmode);
+
+    % Make dummy data
+    datatemp(:,1) = vec_T(:);
+    datatemp(:,2) = 3;
+    datatemp(:,3)  =0.1;
+
+    writedisp_surf96(datatemp,'disp_obs.dsp','R',disp_type,1,nmode); % write temp data into dispersion file 
+    writemod_surf96(model,'start.mod');
+
+    % run surf96 
+
+    [~, log] = system([timeoutstr,'surf96 1 27 temp.dsp']);
+    % read dispersion from tmep file
+    data=readdisp_surf96('temp.dsp');
+
+    if(~isempty(data))
+        phv=data(:,2);
+    else
+        phv=[];
+    end
+    %
+    system('surf96 39'); % clean up
 end
-%
-system('surf96 39'); % clean up
 
 end
 
